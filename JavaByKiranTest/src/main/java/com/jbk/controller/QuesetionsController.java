5ef60b08840988f9874da454b1bb4554dcd6c74d
vikash128 <59698748+vikash128@users.noreply.github.com>
2020-06-10 00:29:05 +0530

    Update QuesetionsController.java

--- JavaByKiranTest/src/main/java/com/jbk/controller/QuesetionsController.java
+++ JavaByKiranTest/src/main/java/com/jbk/controller/QuesetionsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuesetionsController {
 	
 	public Quesetion getQuesetions() {
-		Quesetion quesetion = new Quesetion();
+		Quesetion quesetion = new Quesetion(); // This is just dummy code.
 		quesetion.setQuestionText("Java is plateform independent ?");
 		quesetion.setOptions1("Yes");
 		quesetion.setOptions2("No");
